d60fe50068bdbf26cb567e7686b15590f7b4491b
Vincent Gürtler <vincent.guertler@gmail.com>
2023-01-12 22:49:01 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-
-class CodeEditor extends React.Component {
-  componentDidMount() {
-    this.editor = CodeMirror(this.node, {
-        lineNumbers: true,
-        mode: "python"
-    });
-  }
-
-  render() {
-    return (
-        <form action="{{ url_for('execute') }}" method="post">
-            <textarea ref={node => this.node = node} name="code"></textarea>
-            <br />
-            <input type="submit" value="Execute" />
-        </form>
-    );
-  }
-}
 
 function App() {
-  return <CodeEditor />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
