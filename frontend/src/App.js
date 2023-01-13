6e6cba27ef6a9a351aa4338d7bef3bc8203583d3
Vincent Gürtler <vincent.guertler@gmail.com>
2023-01-13 09:57:35 +0100

    Add theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 import CodeMirror from '@uiw/react-codemirror';
 // import { python } from '@codemirror/lang-python';
-// import { okaidia } from '@uiw/codemirror-theme-okaidia';
+import { okaidia } from '@uiw/codemirror-theme-okaidia';
 
 function App() {
   return (
     <CodeMirror
       value="console.log('hello world!');"
       height="200px"
-      // theme={okaidia}
+      theme={okaidia}
       // extensions={[python]}
     />
   );
