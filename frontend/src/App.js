9cab24802375d12b6307b8268f721e6aeea4d5f3
Vincent Gürtler <vincent.guertler@gmail.com>
2023-01-13 09:56:05 +0100

    Add codemirror

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-// import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror from '@uiw/react-codemirror';
 // import { python } from '@codemirror/lang-python';
 // import { okaidia } from '@uiw/codemirror-theme-okaidia';
 
-// function App() {
-  // return (
-    // <CodeMirror
-      // value="console.log('hello world!');"
-      // height="200px"
+function App() {
+  return (
+    <CodeMirror
+      value="console.log('hello world!');"
+      height="200px"
       // theme={okaidia}
       // extensions={[python]}
-    // />
-  // );
-// }
-
-function App() {
-	return 'salut';
+    />
+  );
 }
 
 export default App;
