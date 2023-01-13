d4d7bab39f426bf85d8274659bf473c5c1e9ad17
Vincent Gürtler <vincent.guertler@gmail.com>
2023-01-13 09:54:00 +0100

    Remove codemirror

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-import CodeMirror from '@uiw/react-codemirror';
-import { python } from '@codemirror/lang-python';
-import { okaidia } from '@uiw/codemirror-theme-okaidia';
+// import CodeMirror from '@uiw/react-codemirror';
+// import { python } from '@codemirror/lang-python';
+// import { okaidia } from '@uiw/codemirror-theme-okaidia';
+
+// function App() {
+  // return (
+    // <CodeMirror
+      // value="console.log('hello world!');"
+      // height="200px"
+      // theme={okaidia}
+      // extensions={[python]}
+    // />
+  // );
+// }
 
 function App() {
-  return (
-    <CodeMirror
-      value="console.log('hello world!');"
-      height="200px"
-      theme={okaidia}
-      extensions={[python]}
-    />
-  );
+	return 'salut';
 }
 
 export default App;
